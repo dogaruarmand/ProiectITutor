e01fb26f81144eb437af03016d9e10bdcba0df40
dogaruarmand <dogaruionelarmand@gmail.com>
2020-01-30 22:06:46 +0200

    tema1, tema2, tema3, tema4

---
+++ tema4/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>Canyon Bikes</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="custom.css">
+ 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+ 	 <script src="javaScript.js"></script> 
+  </head>
+  <body>
+  	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYUF7fjwL2eUTE-r1srPFIo095J-m_J6CxNFhXfSCaQaBLh3MQWQ&s" width="210" height="70" alt="Canyon Logo" style="float: right; margin: 21px 0;">
+  	<br/>
+  	<ul>
+	  <li><a href="#outlet">Outlet categories</a></li>
+	  <li><a href="#gravel">Gravel Bike</a></li>
+	  <li><a href="#components">Bike Components</a></li>
+	  <li><a href="#myBike">My bike</a></li>
+	  <li><a href="#bonus">Bonus</a></li>
+	</ul>
+  	<div style="margin-top: 130px; margin-left: 40px; width: 50%;">
+  		<p style="color: grey; font-family: verdana; font-size: 10px; margin-bottom: 0;">
+  			Canyon // Outlet
+  		</p>
+  		<h1 style="margin-top: 0; text-transform: uppercase;">Canyon Factory Outlet </h1>
+  		<p style="text-align: justify; color: #008B8B; text-indent: 40px;">
+  			Discounted bikes, outstanding performance. Reduced bikes and gear, delivered direct to your door. Save up to 40% on Road, Mountain, Triathlon and Urban bikes and Gear. Special offers on past-season and used bikes, all with the same Canyon quality, guarantee and direct service. 
+  		</p>
+  	</div> 
+  	<table>
+  		<caption id="outlet">Outlet categories</caption>
+  		<tbody>
+  			<!--row1-->
+  			<tr>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet/road-bikes/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw2c7dd2ab/images/specials/outlet-road-v3.jpg?sw=416" alt="Road  Outlet"></a>
+	  				<br/>
+	  				<span>Road Outlet</span>
+	  			</td>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet/mountain-bikes/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw22c2d0af/images/specials/outlet-mountain-v3.jpg?sw=416" alt="MTB Outlet"></a>
+	  				<br/>
+	  				<span>MTB Outlet</span>
+	  			</td>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet/fitness-bikes/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw1ff37fdb/images/specials/outlet-fitness-v3.jpg?sw=416" alt="Fitness Outlet"></a>
+	  				<br/>
+	  				<span>Fitness Outlet</span>
+	  			</td>	  			
+  			</tr>
+  			<!--end row1-->
+  			<!--row2-->
+  			<tr>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet/urban-bikes/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dwa6f9b3c5/images/specials/outlet-urban-v3.jpg?sw=416" alt="Urban  Outlet"></a>
+	  				<br/>
+	  				<span>Urban Outlet</span>
+	  			</td>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet-bikes/pro-bikes/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw7bb8ca23/images/specials/outlet-probikes-v3.jpg?sw=416" alt="Pro Bikes Outlet"></a>
+	  				<br/>
+	  				<span>Pro Bikes Outlet</span>
+	  			</td>
+	  			<td>
+	  				<a href="https://www.canyon.com/en-ro/outlet/gear/"><img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw0cc48f49/images/specials/outlet-gear-v3.jpg?sw=416" alt="Gear Outlet"></a>
+	  				<br/>
+	  				<span>Gear Outlet</span>
+	  			</td>	  			
+  			</tr>
+  			<!--end row2-->
+  		</tbody>  		
+  	</table>
+  	<!--playing with text and image-->
+  	<div style="text-align: center;">
+  		<img src="https://media-cdn.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Library-Sites-canyon-shared/default/dw4d898637/images/homepage/grailcf_my20_hero.jpg?sw=1280" alt="Grail Bike" title="Grail CF SL" style="width: 80%;">
+  		<span id="gravel"></span> 		
+  	</div>  	
+  	<b><p style="font-size: 30px; width: 30%; display: inline-block; margin-left: 130px; text-transform: uppercase;">
+  		The Grail CF SL and CF SLX on sale
+  	</p></b>
+  	<p style="text-align: left; font-family: lucida console; width: 35%; margin-right: 200px; display: inline-block; float: right;">From ultra endurance to bikepacking, gravel puts its own unique set of demands on a bike. With gravel specific gearing, geometry and accessories, the Grail is engineered to meet them all. </p>
+  	<!--List-->
+  	<div>
+  		<h3 id="components">Bike components</h3>  			
+  		<dl style="margin-top: 0; margin-left: 40px; display: inline-block;">
+  			<dt style="font-size: 10px; color: brown;">(click on parts for info)</dt>
+  				<dd>- Front wheel</dd>
+  				<dd>- Back wheel</dd>
+  				<dd>- Frame</dd>
+  				<dd>- Sadle</dd>
+  				<dd>- Front gear</dd>
+  				<dd>- Handlebar</dd>
+  		</dl>
+  		<!--Image map-->
+  		<img src="https://www.gearlimits.com/wp-content/uploads//2018/06/Canyon-Lux-Greber.jpg" alt="Workplace" title="Bicycle" usemap="#bikeComp" width="700" height="450" style="margin-left: 0; margin-right: 290px; margin-bottom: 40px; border: 3px solid grey; float: right; display: inline-block;">
+		<map name="bikeComp">
+		  <area shape="circle" coords="150,270,130" alt="back wheel" title="back wheel" href="https://en.wikipedia.org/wiki/Bicycle_wheel">
+		  <area shape="circle" coords="530,270,130" alt="front wheel" title="front wheel" href="https://en.wikipedia.org/wiki/Bicycle_wheel">
+		  <area shape="rect" coords="280,60,420,255" alt="Frame" title="Frame" href="https://en.wikipedia.org/wiki/Bicycle_frame">
+		  <area shape="rect" coords="200,30,290,55" alt="Sadle" title="Sadle" href="https://en.wikipedia.org/wiki/Bicycle_saddle">
+		  <area shape="circle" coords="320,280,30" alt="Front gear" title="Front gear" href="https://en.wikipedia.org/wiki/Crankset#Chainring">
+		  <area shape="circle" coords="450,30,30" alt="Handlebar" title="Handlebar" href="https://en.wikipedia.org/wiki/Bicycle_handlebar">
+		</map>	
+  	</div> 
+  	<!--some javascript :)--> 	 	
+  	<div style="margin-left: 40px; text-align: center;">
+  		<img src="https://scontent.fcdg2-1.fna.fbcdn.net/v/t1.0-9/s960x960/57313601_584474102056550_7540371020104859648_o.jpg?_nc_cat=106&_nc_ohc=STMDlhLlklQAX9DLc5y&_nc_ht=scontent.fcdg2-1.fna&oh=d02df1dd1159624385bcb81917d77b3b&oe=5ECD114A" style="border: 3px solid black;  width: 70%;">
+  		<p id="myBike" style="color: green; text-shadow: 2px -1px red;">Va place bicicleta mea ?</p>
+  		<button type="button" onclick="da()">Da</button>
+  		<button type="button" onclick="nu()">Nu</button>
+  		<p id="da"></p>
+  	</div>
+  	<!--bonus section  PHP rules :) -->
+  	<div style="width: 50%; text-align: justify; font-size: 18; font-family: georgia; color: grey; margin-left: 50px; text-indent: 30px;">
+  		<p id="bonus" >
+  			Imbinand mersul pe bicicleta cu programarea reusesc sa invat mai usor limbajul <span style="text-decoration: underline; color: red;">PHP</span>, drept pentru care afisez: <span style="color: blue;"><?php  echo "Hello World!";?></span> cu ajutorul bibliotecii PHP. Va recomand sa faceti si voi la fel!
+  		</p>	
+  	</div>  	 
+  	<!--footer-->
+  	<p style="text-align:center; background-color: black; width: 100%; color: white; padding: 10px;">
+        Tema 4 - 2020 @Made by <B><span style="color:red">Dogaru Armand</span></B>@
+    </p>
+  </body>
+</html>
---
+++ tema4/javaScript.js
@@ -0,0 +1,6 @@
+function da() {  
+	document.getElementById('da').innerHTML = 'Multumesc si mie imi place ! :-)'
+}
+function nu() {
+	window.alert('In tema mea nu se accepta rautati, stiam eu ca va place :-)')
+}
