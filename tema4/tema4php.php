d5a88fab0ddf973c58ded9b5576c7818579296ed
dogaruarmand <dogaruionelarmand@gmail.com>
2020-01-31 16:14:21 +0200

    splited index.php in index.html, tema4javaScript.html and tema4php.php

---
+++ tema4/tema4php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>Canyon Bikes</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="custom.css">
+ 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+ 	 <script src="javaScript.js"></script> 
+  </head>
+ <body>
+ <!--bonus section  PHP rules :) -->
+  	<div style="width: 50%; text-align: justify; font-size: 18; font-family: georgia; color: grey; margin-left: 50px; text-indent: 30px;">
+  		<p>
+  			Imbinand mersul pe bicicleta cu programarea reusesc sa invat mai usor limbajul <span style="text-decoration: underline; color: red;">PHP</span>, drept pentru care afisez: <span style="color: blue;"><?php  echo "Hello World!";?></span> cu ajutorul bibliotecii PHP. Va recomand sa faceti si voi la fel!
+  		</p>	
+  	</div>
+  	<a href="index.html">Home</a>
+  	<!--footer-->
+  	<p style="text-align:center; background-color: black; width: 100%; color: white; padding: 10px;">
+        Tema 4 - 2020 @Made by <B><span style="color:red">Dogaru Armand</span></B>@
+    </p>
+  </body>
+</html>
