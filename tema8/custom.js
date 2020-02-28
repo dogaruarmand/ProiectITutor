e5660252f8edc8933e4a6a522083b6c3e5a8046f
dogaruarmand <dogaruionelarmand@gmail.com>
2020-02-29 00:18:28 +0200

    html

--- tema8/custom.js
+++ tema8/custom.js
@@ -1,20 +1,20 @@
-var myVideo = document.getElementById("canyon-video"); 
-
+var myVideo = document.getElementById("canyon-video");
+var h2Video = document.getElementById("video-title");
 function playPause() { 
   if (myVideo.paused) 
     myVideo.play(); 
   else 
     myVideo.pause(); 
-} 
-
+}
 function makeBig() { 
     myVideo.width = 720; 
-} 
-
+    h2Video.style.width = "720px"; 
+}
 function makeSmall() { 
     myVideo.width = 320; 
-} 
-
+    h2Video.style.width = "320px"; 
+}
 function makeNormal() { 
     myVideo.width = 520; 
+    h2Video.style.width = "520px"; 
 }
